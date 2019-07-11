e4d70ec2724dffcb8bdd66c13a03709e5e463d5d
Sufian <sufian.bb@hotmail.com>
2019-07-11 14:57:45 +0200

    id parameter regexp added

--- api/router.go
+++ api/router.go
@@ -15,8 +15,8 @@ func newRouter() http.Handler {
 
 	router.HandleFunc("/api/coupons", handlers.ListCouponsHandler).Methods("GET")
 	router.HandleFunc("/api/coupons", handlers.CreateCouponHandler).Methods("POST")
-	router.HandleFunc("/api/coupons/{id}", handlers.GetCouponHandler).Methods("GET")
-	router.HandleFunc("/api/coupons/{id}", handlers.UpdateCouponHandler).Methods("PUT")
+	router.HandleFunc("/api/coupons/{id:[0-9]+}", handlers.GetCouponHandler).Methods("GET")
+	router.HandleFunc("/api/coupons/{id:[0-9]+}", handlers.UpdateCouponHandler).Methods("PUT")
 
 	return gorillaHandlers.LoggingHandler(os.Stdout, router)
 }
